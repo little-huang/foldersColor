65e472e8d03d6288065c0b67ea2a90354ec2e6e8
little-huang <1124778999@qq.com>
2019-12-17 22:55:13 +0800

    Initialize the folderColor project

---
+++ FolderColor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FolderColor
+//
+//  Created by 黄宝成 on 2019/5/17.
+//  Copyright © 2019 黄宝成. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
